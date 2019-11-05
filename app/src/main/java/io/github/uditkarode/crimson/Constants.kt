ad875bab591095d4a4dd47f68d89ec1a249c6146
Udit Karode <udit.karode@gmail.com>
2019-11-05 13:31:34 +0000

    Initial commit

---
+++ app/src/main/java/io/github/uditkarode/crimson/Constants.kt
@@ -0,0 +1,9 @@
+package io.github.uditkarode.crimson
+
+class Constants {
+    companion object {
+        const val HOST_REFERENCE = "https://raw.githubusercontent.com/uditkarode/crimson/ten-minimal/host"
+        const val KERNEL_NAME = "Crimson"
+        const val CURRENT_YEAR = "2019"
+    }
+}
---
+++ app/src/main/java/io/github/uditkarode/crimson/MainActivity.kt
@@ -0,0 +1,194 @@
+package io.github.uditkarode.crimson
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.androidnetworking.interfaces.StringRequestListener
+import com.onesignal.OneSignal
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+import io.github.inflationx.viewpump.ViewPumpContextWrapper
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var host: String
+    lateinit var sp: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AndroidNetworking.initialize(this@MainActivity)
+
+        OneSignal.startInit(this)
+            .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+            .unsubscribeWhenNotificationsAreDisabled(true)
+            .init()
+
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("RobotoMono-Regular.ttf")
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                ).build()
+        )
+
+        setContentView(R.layout.activity_main)
+        sp = getSharedPreferences("kernelStats", 0)
+        telegramFooter.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/crimsonviolet"))) }
+        githubFooter.setOnClickListener { startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/uditkarode/crimson"))) }
+
+        val kernelVersion = readKernelVersion()
+
+        if(areStatsStored()){
+            codeNameTv.text = sp.getString("codeName", "Clear app data")
+            cafTagTv.text = sp.getString("cafTag", "Clear app data")
+            linuxVersionTv.text = sp.getString("linuxVersion", "Clear app data")
+            buildDateTv.text = sp.getString("buildDate", "Clear app data")
+        }
+
+        if(!kernelVersion.contains(Constants.KERNEL_NAME.toLowerCase(Locale.getDefault()))){
+            Toast.makeText(this@MainActivity, "You are not using the ${Constants.KERNEL_NAME} kernel!", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
+        val buildDate = SimpleDateFormat("MMM dd HH yyyy", Locale.ENGLISH).parse(kernelVersion.substring(kernelVersion.lastIndexOf("PREEMPT")).substring(12, 20) + " ${Constants.CURRENT_YEAR}")
+
+        AndroidNetworking
+            .get(Constants.HOST_REFERENCE)
+            .doNotCacheResponse()
+            .build()
+            .getAsString(object: StringRequestListener {
+                override fun onResponse(response: String) {
+                    host = response
+                    checkForUpdates(buildDate)
+                }
+
+                override fun onError(anError: ANError?) {
+                    Toast.makeText(this@MainActivity, "Please check your internet connection.", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            })
+    }
+
+    private fun stopLoading(updateAvailable: Boolean, downloadLink: String = ""){
+        loader.visibility = View.GONE
+
+        if(updateAvailable){
+            updateStatusImg.setImageDrawable(getDrawable(R.drawable.crossfix))
+            updateStatusTv.text = "An update is available!"
+            updateButton.visibility = View.VISIBLE
+            updateButton.setOnClickListener {
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(downloadLink)))
+            }
+        }
+
+        content.visibility = View.VISIBLE
+    }
+
+    private fun areStatsStored() = sp.getString("codeName", "rip") != "rip"
+
+    private fun checkForUpdates(buildDate: Date){
+        AndroidNetworking
+            .get("$host/latest.json")
+            .doNotCacheResponse()
+            .build()
+            .getAsJSONObject(object: JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    val latestDate = SimpleDateFormat("MMM dd HH yyyy", Locale.ENGLISH).parse(response.getString("builtAt") + " ${Constants.CURRENT_YEAR}")
+                    if(latestDate.after(buildDate)){
+                        val codeName = response.getString("codeName")
+                        val cafTag = response.getString("cafTag")
+                        val linuxVersion = response.getString("linuxVersion")
+                        val buildFdate = SimpleDateFormat("dd/MM/yyyy hh a", Locale.ENGLISH).format(latestDate)
+
+                        val editor = sp.edit()
+                        editor.putString("codeName", codeName)
+                        editor.putString("cafTag", cafTag)
+                        editor.putString("linuxVersion", linuxVersion)
+                        editor.putString("buildDate", buildFdate)
+                        editor.apply()
+
+                        if(!areStatsStored()) {
+                            codeNameTv.text = "Update available"
+                            cafTagTv.text = "Update available"
+                            linuxVersionTv.text = "Update available"
+                            buildDateTv.text = "Update available"
+                        }
+
+                        updateBuildDateTv.text = "built at: $buildFdate"
+                        updateBuildDateTv.visibility = View.VISIBLE
+                        stopLoading(true, response.getString("downloadLink"))
+                    } else {
+                        stopLoading(false)
+                        if(!areStatsStored()){
+                            val codeName = response.getString("codeName")
+                            val cafTag = response.getString("cafTag")
+                            val linuxVersion = response.getString("linuxVersion")
+                            val latestFDate = SimpleDateFormat("dd/mm/yyyy hh a", Locale.ENGLISH).format(latestDate)
+                            val editor = sp.edit()
+                            editor.putString("codename", codeName)
+                            editor.putString("caftag", cafTag)
+                            editor.putString("linuxVersion", linuxVersion)
+                            editor.putString("buildDate", latestFDate)
+                            editor.apply()
+
+                            codeNameTv.text = codeName
+                            cafTagTv.text = cafTag
+                            linuxVersionTv.text = linuxVersion
+                            buildDateTv.text = latestFDate
+                        }
+                    }
+                }
+
+                override fun onError(anError: ANError?) {
+                    Toast.makeText(this@MainActivity, "Please check your internet connection! ${anError?.errorDetail.toString()}", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            })
+    }
+
+    override fun attachBaseContext(newBase: Context) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase))
+    }
+
+    private fun readKernelVersion(): String {
+        try {
+            val p = Runtime.getRuntime().exec("uname -av")
+            val `is`: InputStream? = if (p.waitFor() == 0) {
+                p.inputStream
+            } else {
+                p.errorStream
+            }
+            val br = BufferedReader(
+                InputStreamReader(`is`),
+                32
+            )
+            val line = br.readLine()
+            br.close()
+            return line
+        } catch (ex: Exception) {
+            return "ERROR: " + ex.message
+        }
+
+    }
+}
